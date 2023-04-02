475f2b24ce248c604b1c413f9da496f49b343985
bagusbpg <bagusbpg@gmail.com>
2023-04-02 09:42:23 +0700

    feat(tenpo.kikai.daemon): initial implementation of daemon

---
+++ kikai/daemon/daemon.go
@@ -0,0 +1,148 @@
+package daemon
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/exp/slog"
+)
+
+type Component interface {
+	New(config interface{}) error
+	Start() error
+	Stop() error
+}
+
+type Runner interface {
+	Run(component Component, config interface{})
+}
+
+type runner struct{}
+
+func NewServiceRunner() Runner {
+	return &runner{}
+}
+
+func (ths *runner) Run(component Component, config interface{}) {
+	ctx := context.Background()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	terminated := make(chan struct{}, 1)
+	go func() {
+		sig := <-c
+		slog.LogAttrs(
+			ctx,
+			slog.LevelInfo, "exiting service",
+			slog.String("receivingSignal", sig.String()),
+		)
+
+		if err := component.Stop(); err != nil {
+			slog.LogAttrs(
+				ctx,
+				slog.LevelError, "failed stopping service",
+				slog.String("causedBy", err.Error()),
+			)
+		}
+
+		close(terminated)
+	}()
+
+	envMode := os.Getenv("ENV_MODE")
+	if !isValidEnvMode(envMode) {
+		slog.LogAttrs(
+			ctx,
+			slog.LevelError, "invalid environment mode",
+			slog.String("ENV_MODE", envMode),
+		)
+		return
+	}
+
+	err := loadConfig(&config, envMode)
+	if err != nil {
+		slog.LogAttrs(
+			ctx,
+			slog.LevelError, "failed loading config",
+			slog.String("causedBy", err.Error()),
+		)
+
+		return
+	}
+
+	err = component.New(config)
+	if err != nil {
+		slog.LogAttrs(
+			ctx,
+			slog.LevelError, "failed initiating service",
+			slog.String("causedBy", err.Error()),
+		)
+
+		return
+	}
+
+	err = component.Start()
+	if err != nil {
+		slog.LogAttrs(
+			ctx,
+			slog.LevelError, "failed starting service",
+			slog.String("causedBy", err.Error()),
+		)
+
+		err = component.Stop()
+		if err != nil {
+			slog.LogAttrs(
+				ctx,
+				slog.LevelError, "failed stopping service",
+				slog.String("causedBy", err.Error()),
+			)
+		}
+
+		close(terminated)
+	} else {
+		slog.LogAttrs(
+			ctx,
+			slog.LevelInfo, "service started",
+			slog.String("ENV_MODE", envMode),
+		)
+	}
+
+	<-terminated
+	slog.LogAttrs(
+		ctx,
+		slog.LevelInfo, "service stopped gracefully",
+	)
+}
+
+func isValidEnvMode(envMode string) bool {
+	switch envMode {
+	case "dev", "stg", "prod":
+		return true
+	default:
+		return false
+	}
+}
+
+func loadConfig(config interface{}, envMode string) error {
+	file := fmt.Sprintf("./config/%s.json", envMode)
+	jsonFile, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("error opening file [%s], cause: %s", file, err)
+	}
+	defer jsonFile.Close()
+
+	jsonByte, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return fmt.Errorf("error reading file [%s], cause: %s", file, err)
+	}
+
+	err = json.Unmarshal(jsonByte, config)
+	if err != nil {
+		return fmt.Errorf("error unmarshal json, cause: %s", err)
+	}
+
+	return nil
+}
